4c0380536c0a2f2bd87253ae095a67149e00d4c6
Jesse Prevallet <japrevallet@gmail.com>
2019-12-09 17:37:54 -0700

    Added Lecturers Model into database and created Lecturer Controller/Views

--- SchoolManagement/Models/SchoolManagementDBModel.Context.cs
+++ SchoolManagement/Models/SchoolManagementDBModel.Context.cs
@@ -28,5 +28,10 @@ namespace SchoolManagement.Models
         public virtual DbSet<Course> Courses { get; set; }
         public virtual DbSet<Enrollment> Enrollments { get; set; }
         public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<Lecturer> Lecturers { get; set; }
     }
 }
