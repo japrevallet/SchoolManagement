e5686ff14dba0c3a96c8a6645ab7106afa43f7a0
Jesse Prevallet <japrevallet@gmail.com>
2019-12-09 20:49:51 -0700

    Ajax partials for EnrollmentController

---
+++ SchoolManagement/Controllers/EnrollmentsController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SchoolManagement.Models;
+
+namespace SchoolManagement.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private SchoolManagementDBEntities db = new SchoolManagementDBEntities();
+
+        // GET: Enrollments
+        public async Task<ActionResult> Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Student).Include(e => e.Lecturer);
+            return View(await enrollments.ToListAsync());
+        }
+
+        public PartialViewResult _enrollmentPartial(int? courseid)
+        {
+            var enrollments = db.Enrollments.Where(q => q.CourseID == courseid).Include(e => e.Course).Include(e => e.Student);
+            return PartialView(enrollments.ToList());
+        }
+
+        // GET: Enrollments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "LastName");
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "EnrollmentId,Grade,CourseID,StudentID,LecturerID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentID);
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName", enrollment.LecturerID);
+            return View(enrollment);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddStudent([Bind(Include = "CourseID,StudentID")] Enrollment enrollment)
+        {
+            try
+            {
+                var isEnrolled = db.Enrollments.Any(q =>q.CourseID == enrollment.CourseID && q.StudentID == enrollment.StudentID);
+
+                if (ModelState.IsValid && !isEnrolled)
+                {
+                    db.Enrollments.Add(enrollment);
+                    await db.SaveChangesAsync();
+                    return Json(new { IsSuccess = true, Message= "Student Added Successfully"}, JsonRequestBehavior.AllowGet);
+                }
+                return Json(new { IsSuccess = false, Message = "Student was already enrolled." }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+
+                return Json(new { IsSuccess = false, Message = "Error: Please contact Administrator" }, JsonRequestBehavior.AllowGet); ;
+            }
+        }
+
+        // GET: Enrollments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentID);
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName", enrollment.LecturerID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "EnrollmentId,Grade,CourseID,StudentID,LecturerID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseId", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "LastName", enrollment.StudentID);
+            ViewBag.LecturerID = new SelectList(db.Lecturers, "Id", "FirstName", enrollment.LecturerID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            db.Enrollments.Remove(enrollment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public JsonResult GetStudents(string term)
+        {
+            var students = db.Students.Select(q => new
+            {
+                FullName = q.FirstName + " " + q.LastName, 
+                ID = q.StudentId
+
+            }).Where (q => q.FullName.Contains(term));
+            return Json(students, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
